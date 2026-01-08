7cd67dea3fd0ab342943568f64b086dee8e686fe
Raphael Souza Prado <raphaelpradoo@hotmail.com>
2026-01-07 23:55:21 -0300

    Add project files.

---
+++ Desafio01/Exercicio1.cs
@@ -0,0 +1,6 @@
+﻿namespace Desafio01;
+
+public class Exercicio1
+{
+    public string exibeMensagem(string nome) => String.Concat("Olá, ", nome, "! Seja bem vindo!");
+}
---
+++ Desafio01/Program.cs
@@ -0,0 +1,13 @@
+﻿namespace Desafio01;
+class Program
+{
+    static void Main()
+    {
+        Console.WriteLine("Qual seu nome ?");
+        string nome = Console.ReadLine();
+        
+        Exercicio1 exercicio1 = new Exercicio1();
+        Console.WriteLine(exercicio1.exibeMensagem(nome));
+        
+    }
+}
