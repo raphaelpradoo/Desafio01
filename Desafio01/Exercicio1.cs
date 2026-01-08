7e8bef48220ae0581848844822b14e32a10a4db3
Raphael Souza Prado <raphaelpradoo@hotmail.com>
2026-01-08 00:00:14 -0300

    Comentários adicionados

--- Desafio01/Exercicio1.cs
+++ Desafio01/Exercicio1.cs
@@ -2,5 +2,10 @@
 
 public class Exercicio1
 {
+    /// <summary>
+    /// Função que exibe uma mensagem de boas vindas com o nome inserido pelo usuário
+    /// </summary>
+    /// <param name="nome"></param>
+    /// <returns></returns>
     public string exibeMensagem(string nome) => String.Concat("Olá, ", nome, "! Seja bem vindo!");
 }
