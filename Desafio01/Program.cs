30a0cd15349b5369be9a3498e8c03bada935aa19
Raphael Souza Prado <raphaelpradoo@hotmail.com>
2026-01-08 00:57:47 -0300

    Chamando os Exercicios 1 e 2 na função Main()

--- Desafio01/Program.cs
+++ Desafio01/Program.cs
@@ -3,11 +3,20 @@ class Program
 {
     static void Main()
     {
-        Console.WriteLine("Qual seu nome ?");
-        string nome = Console.ReadLine();
+        #region Exercicio 01
         
         Exercicio1 exercicio1 = new Exercicio1();
-        Console.WriteLine(exercicio1.exibeMensagem(nome));
+        Console.WriteLine(exercicio1.exibeMensagem());
+        Console.WriteLine("****** Fim do Exercicio 1 ****** \n");
         
+        #endregion
+
+        #region Exercicio 02        
+
+        Exercicio2 exercicio2 = new Exercicio2();
+        Console.WriteLine(exercicio2.nomeCompleto());
+        Console.WriteLine("****** Fim do Exercicio 2 ****** \n");
+
+        #endregion
     }
 }
